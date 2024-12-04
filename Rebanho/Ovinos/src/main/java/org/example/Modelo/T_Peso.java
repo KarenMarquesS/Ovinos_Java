15c73d833b20525226ced54ad5b4edb351093588
Karen Marques <kamarquess@yahoo.com.br>
2024-12-04 19:07:35 -0300

    Update T_Peso.java

--- Rebanho/Ovinos/src/main/java/org/example/Modelo/T_Peso.java
+++ Rebanho/Ovinos/src/main/java/org/example/Modelo/T_Peso.java
@@ -13,5 +13,147 @@ public class T_Peso extends EntidadeBase{
     private double peso_90_dias;
     private transient T_Registro registro;
 
+    public T_Peso() {
+    }
+
+    public T_Peso(int id_cordeiro) {
+        super(id_cordeiro);
+    }
+
+    public T_Peso(int id_cordeiro, int n_brinco, double peso_15_dias, double peso_20_dias, double peso_30_dias, double peso_40_dias, double peso_50_dias, double peso_60_dias, double peso_90_dias, T_Registro registro) {
+        this.id_cordeiro = id_cordeiro;
+        this.n_brinco = n_brinco;
+        this.peso_15_dias = peso_15_dias;
+        this.peso_20_dias = peso_20_dias;
+        this.peso_30_dias = peso_30_dias;
+        this.peso_40_dias = peso_40_dias;
+        this.peso_50_dias = peso_50_dias;
+        this.peso_60_dias = peso_60_dias;
+        this.peso_90_dias = peso_90_dias;
+        this.registro = registro;
+    }
+
+    public T_Peso(int id_cordeiro, int id_cordeiro1, int n_brinco, double peso_15_dias, double peso_20_dias, double peso_30_dias, double peso_40_dias, double peso_50_dias, double peso_60_dias, double peso_90_dias, T_Registro registro) {
+        super(id_cordeiro);
+        this.id_cordeiro = id_cordeiro1;
+        this.n_brinco = n_brinco;
+        this.peso_15_dias = peso_15_dias;
+        this.peso_20_dias = peso_20_dias;
+        this.peso_30_dias = peso_30_dias;
+        this.peso_40_dias = peso_40_dias;
+        this.peso_50_dias = peso_50_dias;
+        this.peso_60_dias = peso_60_dias;
+        this.peso_90_dias = peso_90_dias;
+        this.registro = registro;
+    }
+
+    public T_Peso(double peso_15_dias, double peso_20_dias, double peso_30_dias, double peso_40_dias, double peso_50_dias, double peso_60_dias, double peso_90_dias) {
+        this.peso_15_dias = peso_15_dias;
+        this.peso_20_dias = peso_20_dias;
+        this.peso_30_dias = peso_30_dias;
+        this.peso_40_dias = peso_40_dias;
+        this.peso_50_dias = peso_50_dias;
+        this.peso_60_dias = peso_60_dias;
+        this.peso_90_dias = peso_90_dias;
+    }
+
+    @Override
+    public int getId_cordeiro() {
+        return id_cordeiro;
+    }
+
+    @Override
+    public void setId_cordeiro(int id_cordeiro) {
+        this.id_cordeiro = id_cordeiro;
+    }
+
+    public int getN_brinco() {
+        return n_brinco;
+    }
+
+    public void setN_brinco(int n_brinco) {
+        this.n_brinco = n_brinco;
+    }
+
+    public double getPeso_15_dias() {
+        return peso_15_dias;
+    }
+
+    public void setPeso_15_dias(double peso_15_dias) {
+        this.peso_15_dias = peso_15_dias;
+    }
+
+    public double getPeso_20_dias() {
+        return peso_20_dias;
+    }
+
+    public void setPeso_20_dias(double peso_20_dias) {
+        this.peso_20_dias = peso_20_dias;
+    }
+
+    public double getPeso_30_dias() {
+        return peso_30_dias;
+    }
+
+    public void setPeso_30_dias(double peso_30_dias) {
+        this.peso_30_dias = peso_30_dias;
+    }
+
+    public double getPeso_40_dias() {
+        return peso_40_dias;
+    }
+
+    public void setPeso_40_dias(double peso_40_dias) {
+        this.peso_40_dias = peso_40_dias;
+    }
+
+    public double getPeso_50_dias() {
+        return peso_50_dias;
+    }
+
+    public void setPeso_50_dias(double peso_50_dias) {
+        this.peso_50_dias = peso_50_dias;
+    }
+
+    public double getPeso_60_dias() {
+        return peso_60_dias;
+    }
+
+    public void setPeso_60_dias(double peso_60_dias) {
+        this.peso_60_dias = peso_60_dias;
+    }
+
+    public double getPeso_90_dias() {
+        return peso_90_dias;
+    }
+
+    public void setPeso_90_dias(double peso_90_dias) {
+        this.peso_90_dias = peso_90_dias;
+    }
+
+    public T_Registro getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(T_Registro registro) {
+        this.registro = registro;
+    }
+
+    @Override
+    public String toString() {
+        return "T_Peso{" +
+                "id_cordeiro=" + id_cordeiro +
+                ", n_brinco=" + n_brinco +
+                ", peso_15_dias=" + peso_15_dias +
+                ", peso_20_dias=" + peso_20_dias +
+                ", peso_30_dias=" + peso_30_dias +
+                ", peso_40_dias=" + peso_40_dias +
+                ", peso_50_dias=" + peso_50_dias +
+                ", peso_60_dias=" + peso_60_dias +
+                ", peso_90_dias=" + peso_90_dias +
+                ", registro=" + registro +
+                ", id_cordeiro=" + id_cordeiro +
+                '}';
+    }
 
 }
